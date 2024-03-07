8f37dfb8b7dd3fcb45f4bdb16fdd9f07d6b5f236
underpickled <dylan@geocene.com>
2024-03-06 16:57:06 -0800

    Reduce logging

--- subsys/fs/fcb/fcb.c
+++ subsys/fs/fcb/fcb.c
@@ -17,7 +17,7 @@
 
 #include <zephyr/logging/log.h>
 
-LOG_MODULE_REGISTER(FCB_INT, LOG_LEVEL_INF);
+LOG_MODULE_REGISTER(FCB_INT, LOG_LEVEL_WRN);
 
 uint16_t fcb_get_sector_idx(struct fcb *fcb, struct flash_sector *sector)
 {
